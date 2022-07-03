38f6976fe1d74c8d164cc344472a3b5025186fc3
ahtrahdis7 <sidharthabiki@gmail.com>
2022-07-03 23:13:57 +0530

    added and updated themes and basic setup

--- App.tsx
+++ App.tsx
@@ -1,87 +1,91 @@
 import React from "react";
 import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
   NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
+  useColorMode
 } from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from 'react-native-vector-icons';
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
+import screens from './screens';
+import ProStatusBar from './components/StatusBar';
+import theme from './theme';
 
-// extend the theme
-export const theme = extendTheme({ config });
-type MyThemeType = typeof theme;
-declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
+const Tab = createBottomTabNavigator();
+
+function Navigator() {
+  // const { colors } = useTheme();
+  const { colorMode } = useColorMode();
+  return (
+    <NavigationContainer>
+        <Tab.Navigator 
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'ios-information-circle-sharp'
+                  : 'ios-information-circle-outline';
+              } else if (route.name === 'Profile') {
+                iconName = focused ? 'person-circle-sharp' : 'person-circle-outline';
+              } else if (route.name === 'Notifications') {
+                iconName = focused ? 'notifications-circle-sharp' : 'notifications-circle-outline';
+              } else if (route.name === 'Messages') {
+                iconName = focused ? 'document-text-sharp' : 'document-text-outline';
+              }
+  
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: colorMode == 'light' ? '#0f172a' : '#f8fafc',
+            tabBarActiveBackgroundColor: colorMode == 'light' ? '#e2e8f0' : '#1e293b',
+            tabBarInactiveBackgroundColor: colorMode == 'light' ? '#f8fafc' : '#0f172a',
+            tabBarInactiveTintColor: colorMode == 'light' ? '#0f172a' : '#f8fafc',
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={screens.Home}
+            options={optionsHandler}
+          />
+          <Tab.Screen 
+            name="Messages" 
+            component={screens.Messages}
+            options={optionsHandler}
+           />
+          <Tab.Screen 
+            name="Profile" 
+            component={screens.Profile}             
+            options={optionsHandler}
+          />
+          <Tab.Screen 
+            name="Notifications" 
+            component={screens.Notifications}             
+            options={optionsHandler}
+          />
+        </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
+
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+    <NativeBaseProvider theme={theme}>
+      <ProStatusBar />
+      <Navigator />
     </NativeBaseProvider>
   );
 }
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+function optionsHandler(props: any) {
+  const { colorMode } = useColorMode();
+  return { 
+    title: props.title,
+    headerStyle: {
+      backgroundColor: colorMode == 'light' ? '#f8fafc' : '#0f172a',
+    },
+    headerTintColor: colorMode == 'light' ? '#0f172a' : '#f8fafc',
+  }
 }
+
